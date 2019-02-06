9d7a3d4df9bf3516462e47e347103efb2aa4b79a
Svetozar Stojkovic <svetozar.cvele.stojkovic@gmail.com>
2019-02-06 17:28:23 +0100

    [init][l] data + datapackage + script

---
+++ README.md
@@ -0,0 +1,40 @@
+This dataset was scraped from [London data](https://data.london.gov.uk/) website.
+Life expectancy at birth and age 65 by sex.
+Data for 2000-2002 to 2008-2010 revised on 24 July 2013.
+Local authorities based on boundaries as of 2010.
+England and Wales figures - non-resident deaths included.
+Figures given for 3 combined years to increase reliability at local levels.
+
+## Data
+Dataset used for this scraping have been found on [Life Expectancy at Birth and at Age 65, Borough](https://data.london.gov.uk/dataset/life-expectancy-birth-and-age-65-borough).
+ 
+Output data is located in `data` directory, it consists of three `csv` files:
+* `male-life-expectancy.csv`
+* `female-life-expectancy.csv`
+* `life-expectancy-at-65.csv`
+
+## Preparation
+You will need Python 3.6 or greater and dataflows library to run the script
+
+To update the data run the process script locally:
+
+```
+# Install dataflows
+pip install dataflows
+
+# Run the script
+python london-life-expectancy.py
+```
+
+### License
+
+Open Government Licence
+
+> You are encouraged to use and re-use the Information that is available under this licence freely and flexibly, with only a few conditions.
+Using Information under this licence
+>Use of copyright and database right material expressly made available under this licence (the 'Information') indicates your acceptance of the terms and conditions below.
+> The Licensor grants you a worldwide, royalty-free, perpetual, non-exclusive licence to use the Information subject to the conditions below.
+> This licence does not affect your freedom under fair dealing or fair use or any other copyright or database right exceptions and limitations.
+
+You may find further information [here](http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/)
+
---
+++ london-life-expectancy.py
@@ -0,0 +1,89 @@
+from dataflows import Flow, load, dump_to_path, ResourceWrapper, PackageWrapper, unpivot
+
+
+def set_format_and_name(package: PackageWrapper):
+
+    package.pkg.descriptor['title'] = 'Health - Life expectancy'
+    package.pkg.descriptor['name'] = 'health-life-expectancy'
+
+    package.pkg.descriptor['licenses'] = [{
+        "name": "OGL v2",
+        "path": 'http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2/',
+        "title": 'UK Open Government Licence'
+    }]
+
+    package.pkg.descriptor['resources'][0]['path'] = 'data/male-life-expectancy.csv'
+    package.pkg.descriptor['resources'][0]['name'] = 'male-life-expectancy'
+
+    package.pkg.descriptor['resources'][1]['path'] = 'data/female-life-expectancy.csv'
+    package.pkg.descriptor['resources'][1]['name'] = 'female-life-expectancy'
+
+    package.pkg.descriptor['resources'][2]['path'] = 'data/life-expectancy-at-65.csv'
+    package.pkg.descriptor['resources'][2]['name'] = 'life-expectancy-at-65'
+
+    yield package.pkg
+    res_iter = iter(package)
+    first: ResourceWrapper = next(res_iter)
+    second: ResourceWrapper = next(res_iter)
+    third: ResourceWrapper = next(res_iter)
+
+    yield first.it
+    yield second.it
+    yield third.it
+
+    yield from package
+
+
+link = 'https://data.london.gov.uk/download/life-expectancy-birth-and-age-65-borough/' \
+       'b37eea33-4f3b-4c54-8590-4b68ba241165/life-expectancy-birth-over65-borough.xls'
+
+unpivot_fields = [
+    {'name': '1991-1993', 'keys': {'Year': '1991-1993'}},
+    {'name': '1992-1994', 'keys': {'Year': '1992-1994'}},
+    {'name': '1993-1995', 'keys': {'Year': '1993-1995'}},
+    {'name': '1994-1996', 'keys': {'Year': '1994-1996'}},
+    {'name': '1995-1997', 'keys': {'Year': '1995-1997'}},
+    {'name': '1996-1998', 'keys': {'Year': '1996-1998'}},
+    {'name': '1997-1999', 'keys': {'Year': '1997-1999'}},
+    {'name': '1998-2000', 'keys': {'Year': '1998-2000'}},
+    {'name': '1999-2001', 'keys': {'Year': '1999-2001'}},
+    {'name': '2000-2002', 'keys': {'Year': '2000-2002'}},
+    {'name': '2001-2003', 'keys': {'Year': '2001-2003'}},
+    {'name': '2002-2004', 'keys': {'Year': '2002-2004'}},
+    {'name': '2003-2005', 'keys': {'Year': '2003-2005'}},
+    {'name': '2004-2006', 'keys': {'Year': '2004-2006'}},
+    {'name': '2005-2007', 'keys': {'Year': '2005-2007'}},
+    {'name': '2006-2008', 'keys': {'Year': '2006-2008'}},
+    {'name': '2007-2009', 'keys': {'Year': '2007-2009'}},
+    {'name': '2008-2010', 'keys': {'Year': '2008-2010'}},
+    {'name': '2009-2011', 'keys': {'Year': '2009-2011'}},
+    {'name': '2010-2012', 'keys': {'Year': '2010-2012'}},
+    {'name': '2011-2013', 'keys': {'Year': '2011-2013'}},
+    {'name': '2012-2014', 'keys': {'Year': '2012-2014'}}
+]
+extra_keys = [
+    {'name': 'Year', 'type': 'any'}
+]
+extra_value = {'name': 'Value', 'type': 'any'}
+
+Flow(
+    load(link,
+         format="xls",
+         skip_rows=[''],
+         fill_merged_cells=True,
+         sheet=2),
+    load(link,
+         format="xls",
+         skip_rows=[''],
+         fill_merged_cells=True,
+         sheet=3),
+    unpivot(unpivot_fields, extra_keys, extra_value),
+    load(link,
+         format="xls",
+         headers=[1, 2],
+         skip_rows=[''],
+         fill_merged_cells=True,
+         sheet=4),
+    set_format_and_name,
+    dump_to_path(),
+).process()
